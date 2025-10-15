9e14ca188347e3e83fb239f9b8100a4e01b2c2e5
Adriano <adricatena@gmail.com>
2025-10-14 21:40:14 -0300

    chore: update Next.js configuration to include custom webpack settings and standalone output

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  webpack: (webpackConfig) => {
+    webpackConfig.resolve.extensionAlias = {
+      '.cjs': ['.cts', '.cjs'],
+      '.js': ['.ts', '.tsx', '.js', '.jsx'],
+      '.mjs': ['.mts', '.mjs'],
+    }
+
+    return webpackConfig
+  },
+  output: 'standalone',
 }
 
-export default withPayload(nextConfig)
+export default withPayload(nextConfig, { devBundleServerPackages: false })
