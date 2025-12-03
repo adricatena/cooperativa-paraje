8e9aba3fb909798f08f5a19171e835d59223544c
Adriano <adricatena@gmail.com>
2025-12-03 19:17:45 -0300

    chore: add Next.js configuration file and update TypeScript settings for improved compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import { withPayload } from '@payloadcms/next/withPayload'
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+ 
+const nextConfig:  NextConfig = {
   // Your Next.js config here
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
